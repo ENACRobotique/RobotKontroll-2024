ffa1216a82501ecc3a83030d40f0ceb937685cc4
ENACRobotique <enac.robotique@github.com>
2025-11-27 16:54:16 +0100

    Ajout de la possibilité de tracer des trajectoires + chgmt nom des images

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QImage, QPainter, QColor, QPen, QTransform
-from PyQt5.QtCore import QRectF, Qt, QObject, pyqtSignal, QLocale, QPointF
+from PyQt5.QtCore import QRectF, Qt, QObject, pyqtSignal, QLocale, QPointF, QLineF
 import math
+import numpy as np
 
 import sys
 
 from PyQt5.QtWidgets import QGraphicsSceneMouseEvent
 
-import generated.robot_state_pb2 as hgpb
-import ecal.core.core as ecal_core
-from ecal.core.publisher import ProtoPublisher
-from ecal.core.subscriber import ProtoSubscriber
+import generated.common_pb2 as hgpb
+import ecal.nanobind_core as ecal_core
+from ecal.msg.proto.core import Subscriber as ProtoSubscriber
+from ecal.msg.proto.core import Publisher as ProtoPublisher
+from ecal.msg.common.core import ReceiveCallbackData
 
 
 Robot_diameter = 300 #mm
@@ -20,18 +22,38 @@ Map_width = 3000 #mm
 Scale = 0.43 # mm -> graphic_size
 
 
+
 class Map_scene(QGraphicsScene):
     def __init__(self,x,y,w,h):
         super().__init__(x,y,w,h)
-        self.background = QImage("table.png")
-        self.pub_send_pos = ProtoPublisher("set_position",hgpb.Position)
+        self.background = QImage("table2025.png")
+        self.pub_send_pos = ProtoPublisher(hgpb.Position,"set_position")
+        self.pub_send_trajectoire = ProtoPublisher(hgpb.Trajectoire,"set_trajectoire")
         self.selectedRobotGraphic = None
         self.pressPoint = QPointF()
         self.orientation_line = QGraphicsLineItem(0,0,Map_width * Scale, Map_height * Scale)
         self.addItem(self.orientation_line)
         self.orientation_line.hide()
         self.orientation_line.setPen(QPen(Qt.blue, 2, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
-        
+
+        self.trajectoire = []
+        # self.last_trajectoire_line = QGraphicsLineItem(0,0,Map_width * Scale, Map_height * Scale)
+        # self.addItem(self.last_trajectoire_line)
+        # self.last_trajectoire_line.hide()
+        self.last_trajectoire_line = None
+        # self.last_trajectoire_line.setPen(QPen(Qt.green, 2, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+
+
+    def _to_table_ref(self,point:QPointF):
+        return (point.x() / Scale, (Map_height* Scale - point.y()) / Scale)
+    
+    @staticmethod
+    def center_rad(theta):
+        while theta > math.pi:
+            theta -= 2*math.pi
+        while theta < -math.pi:
+            theta += 2*math.pi
+        return theta
 
     def drawBackground(self, painter: QPainter, rect: QRectF) -> None:
         rect_image = QRectF(rect.topLeft().x() / (Map_width*Scale) * self.background.width() , rect.topLeft().y() / (Map_height*Scale) * self.background.height(), self.background.width() * rect.width() / (Map_width*Scale) ,self.background.height() * rect.height() / (Map_height*Scale))
@@ -44,6 +66,11 @@ class Map_scene(QGraphicsScene):
         if event.button() == Qt.MouseButton.LeftButton :
             self.pressPoint = event.scenePos()
             self.orientation_line.show()
+            if self.last_trajectoire_line is not None:
+                p1 = self.last_trajectoire_line.line().p1()
+                self.last_trajectoire_line.setLine(QLineF(p1,event.scenePos()))
+                self.trajectoire.append(self.last_trajectoire_line)
+                self.last_trajectoire_line = None
         elif event.button() == Qt.MouseButton.RightButton :
             items = self.items(event.scenePos(), order = Qt.SortOrder.AscendingOrder)
             try :
@@ -52,38 +79,73 @@ class Map_scene(QGraphicsScene):
                 print("No robot at this place")
             if self.selectedRobotGraphic != None:
                 print(f'{self.selectedRobotGraphic.name}_{self.selectedRobotGraphic.rep_name} selected')
-
+        elif event.button() == Qt.MouseButton.MiddleButton :
+            if self.last_trajectoire_line is not None:
+                self.trajectoire.append(self.last_trajectoire_line)
+            self.last_trajectoire_line = QGraphicsLineItem(QLineF(event.scenePos(),event.scenePos()))
+            self.last_trajectoire_line.setPen(QPen(Qt.green, 2, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+            self.addItem(self.last_trajectoire_line)
+            # print(self.trajectoire)
     
     def mouseMoveEvent(self, event: QGraphicsSceneMouseEvent) -> None:
         self.orientation_line.setLine(self.pressPoint.x(), self.pressPoint.y(), event.scenePos().x(), event.scenePos().y())
+        if self.last_trajectoire_line is not None:
+            p1 = self.last_trajectoire_line.line().p1()
+            self.last_trajectoire_line.setLine(QLineF(p1,event.scenePos()))
     
     def mouseReleaseEvent(self, event: QGraphicsSceneMouseEvent) -> None:
         if event.button() == Qt.MouseButton.LeftButton:
             self.orientation_line.hide()
+            if len(self.trajectoire) > 0: # Cas où on a une trajectoire
+                def line_to_pos(ligne):
+                    x,y = self._to_table_ref(ligne.line().p1()) 
+                    theta = -math.atan2(ligne.line().dy(),ligne.line().dx())
+                    return hgpb.Position(x=x,y=y,theta=theta)
 
-            def send_pos(x,y,theta):
-                msg = hgpb.Position()
-                msg.x = x
-                msg.y = y
-                msg.theta = theta
-                self.pub_send_pos.send(msg)
+                # Calcul du delta
+                delta = event.scenePos() - self.trajectoire[-1].line().p2()
+                distance = math.sqrt(QPointF.dotProduct(delta,delta))
+                theta = -math.atan2(delta.y(),delta.x()) if distance > 0 else -math.atan2(self.trajectoire[-1].line().dy(),self.trajectoire[-1].line().dx())
 
-            delta = event.scenePos()
-            delta -= self.pressPoint
-            distance = math.sqrt(QPointF.dotProduct(delta,delta))
-            if distance < Scale * 10:
-                if self.selectedRobotGraphic == None:
-                    print("No selected Robot")
-                else :
-                    theta = self.selectedRobotGraphic.theta
+                traj = hgpb.Trajectoire()
+                for ligne in self.trajectoire:
+                    traj.pos.append(line_to_pos(ligne))
+
+                _x,_y = self._to_table_ref(self.trajectoire[-1].line().p2())
+                traj.pos.append(hgpb.Position(x=_x,y=_y,theta=theta))
+
+                self.pub_send_trajectoire.send(traj)
+                print(traj)
+
+                for ligne in self.trajectoire:
+                    self.removeItem(ligne)
+                self.trajectoire=[]
+
+            else : # Cas destination = 1 seul point
+
+                def send_pos(x,y,theta):
+                    msg = hgpb.Position()
+                    msg.x = x
+                    msg.y = y
+                    msg.theta = theta
+                    self.pub_send_pos.send(msg)
+
+                delta = event.scenePos()
+                delta -= self.pressPoint
+                distance = math.sqrt(QPointF.dotProduct(delta,delta))
+                if distance < Scale * 10:
+                    if self.selectedRobotGraphic == None:
+                        print("No selected Robot")
+                    else :
+                        theta = self.selectedRobotGraphic.theta
+                        send_pos(self.pressPoint.x() / Scale, (Map_height* Scale - self.pressPoint.y()) / Scale, theta)
+                        print(self.pressPoint.x() / Scale, (Map_height* Scale - self.pressPoint.y()) / Scale)
+                else:
+                    theta = math.acos(delta.x() / distance)
+                    if delta.y() > 0:
+                        theta *= -1
                     send_pos(self.pressPoint.x() / Scale, (Map_height* Scale - self.pressPoint.y()) / Scale, theta)
-                    print(self.pressPoint.x() / Scale, (Map_height* Scale - self.pressPoint.y()) / Scale)
-            else:
-                theta = math.acos(delta.x() / distance)
-                if delta.y() > 0:
-                    theta *= -1
-                send_pos(self.pressPoint.x() / Scale, (Map_height* Scale - self.pressPoint.y()) / Scale, theta)
-                print(self.pressPoint.x() / Scale, (Map_height* Scale - self.pressPoint.y()) / Scale,theta)
+                    print(self.pressPoint.x() / Scale, (Map_height* Scale - self.pressPoint.y()) / Scale,theta)
     
 
     
@@ -100,9 +162,6 @@ class Monitor_Command(QTabWidget):
         else:
             robot = self.robots[robot_name]
         robot.addRobotGraphic(robot_graphic)
-
-        
-
 
 
 class MySignalEmitter(QObject):
@@ -158,7 +217,7 @@ class Robot:
         theta_send.setRange(0, 360)
         theta_send.setLocale(QLocale("en"))
 
-        pub_send_pos = ProtoPublisher(ecal_topic_send,hgpb.Position)
+        pub_send_pos = ProtoPublisher(hgpb.Position,ecal_topic_send)
 
         def send_pos(x,y,theta):
             msg = hgpb.Position()
@@ -197,7 +256,7 @@ class Robot:
         theta_send.setRange(-360, 360)
         theta_send.setLocale(QLocale("en"))
 
-        #pub_send_speed = ProtoPublisher(ecal_topic_send,hgpb.Position)
+        #pub_send_speed = ProtoPublisher(hgpb.Position,ecal_topic_send)
 
         def send_pos(x,y,theta):
             msg = hgpb.Speed()
@@ -208,7 +267,13 @@ class Robot:
         
         #send.clicked.connect(lambda : send_pos(x_send.value(), y_send.value(), math.radians(theta_send.value())))
     
-    
+def normalize_angle(a):
+    while a > math.pi:
+        a -= 2*math.pi
+    while a < -math.pi:
+        a+= 2*math.pi
+    return a
+
     
 class RobotGraphic(QGraphicsItemGroup):
     def __init__(self, name, rep_name, ecal_pos_topic, color = "darkblue") -> None:
@@ -229,12 +294,16 @@ class RobotGraphic(QGraphicsItemGroup):
         self.pos_frame.setMaximumHeight(50)
         self.pos_frame.setStyleSheet(f"QFrame {{ background : {color}; }}")
         name_label = QLabel(self.rep_name)
+        name_label.setStyleSheet("font-size:28px;")
         self.pos_frame_layout.addWidget(name_label)
         self.x_label = QLabel("x: Na")
+        self.x_label.setStyleSheet("font-size:28px;")
         self.pos_frame_layout.addWidget(self.x_label)
         self.y_label = QLabel("y: Na")
+        self.y_label.setStyleSheet("font-size:28px;")
         self.pos_frame_layout.addWidget(self.y_label)
         self.theta_label = QLabel("theta: Na")
+        self.theta_label.setStyleSheet("font-size:28px;")
         self.pos_frame_layout.addWidget(self.theta_label)
 
         ## GraphicsItem
@@ -250,13 +319,14 @@ class RobotGraphic(QGraphicsItemGroup):
 
         ## Ecal
         # Position
-        def send_pos_signal(topic_name, hlm, time):
+        def send_pos_signal( pub_id : ecal_core.TopicId, data : ReceiveCallbackData[hgpb.Position]):
             #if hlm.name == self.name:
             #    self.signal_emitter.pos_signal.emit(hlm.x, hlm.y, hlm.theta)
-            self.signal_emitter.pos_signal.emit(hlm.x, hlm.y, hlm.theta)
+            self.signal_emitter.pos_signal.emit(data.message.x, data.message.y, data.message.theta)
+            self.signal_emitter.pos_signal.emit(data.message.x, data.message.y, data.message.theta)
 
-        pos_sub = ProtoSubscriber(ecal_pos_topic, hgpb.Position)
-        pos_sub.set_callback(send_pos_signal)
+        self.pos_sub = ProtoSubscriber(hgpb.Position,ecal_pos_topic)
+        self.pos_sub.set_receive_callback(send_pos_signal)
 
         self.signal_emitter.pos_signal.connect(self.setPos)
         
@@ -277,7 +347,7 @@ class RobotGraphic(QGraphicsItemGroup):
         ## Affichage Tabs
         self.x_label.setText("x: {:.3f}".format(x))
         self.y_label.setText("y: {:.3f}".format(y))
-        self.theta_label.setText("theta: {:.3f}".format(math.degrees(theta)))
+        self.theta_label.setText("theta: {:.3f}".format(math.degrees(normalize_angle(theta))))
     
         
 
@@ -289,7 +359,7 @@ def addRobot(scene, tabs, name, representation_name, ecal_pos_topic, color):
 
 
 if __name__ == "__main__":
-    ecal_core.initialize(sys.argv, "Robot2Kontroll")
+    ecal_core.initialize("Robot2Kontroll")
     app = QApplication([])
     main_window = QMainWindow()
 
